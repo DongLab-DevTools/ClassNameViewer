927d763fdca46702c082aca8b4636783510ef7ab
김동현 <dongdong99@kakao.com>
2025-09-14 18:16:45 +0900

    feast : github packages 배포

--- screennameviewer/build.gradle.kts
+++ screennameviewer/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+
+    id("maven-publish")
 }
+
+group = "com.donglab.devtools"
+version = libs.versions.sdk.version.get()
 
 android {
     namespace = "com.donglab.screennameviewer"
@@ -27,6 +34,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -37,4 +51,38 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    repositories {
+        maven {
+            val props = Properties().apply {
+                load(rootProject.file("github.properties").inputStream())
+            }
+            url = uri(props.getProperty("url"))
+
+            credentials {
+                username = props.getProperty("github_username")
+                password = props.getProperty("github_token")
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                artifactId = "screennameviewer"
+
+                // POM Metadata (Optional)
+                pom {
+                    name.set("ScreenNameViewer")
+                    description.set("Screen name viewer library")
+                    url.set("https://github.com/DongLab-DevTools/ScreenNameViewer")
+                }
+            }
+        }
+    }
 }
